2c00900b294d45e6395845d58bb1af8a0dd07e1f
Lance <754539910@qq.com>
2015-08-28 18:04:48 +0800

    sql

--- frame.sql
+++ frame.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2015 年 08 月 23 日 16:07
+-- 生成日期: 2015 年 08 月 28 日 10:04
 -- 服务器版本: 5.5.20
 -- PHP 版本: 5.3.10
 
@@ -23,15 +23,49 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- 表的结构 `tb_user`
+-- 表的结构 `cms_category`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `pass` varchar(32) NOT NULL,
+CREATE TABLE IF NOT EXISTS `cms_category` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(20) NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  `intro` varchar(100) NOT NULL,
+  PRIMARY KEY (`category_id`),
+  KEY `parent_id` (`parent_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+
+--
+-- 转存表中的数据 `cms_category`
+--
+
+INSERT INTO `cms_category` (`category_id`, `category_name`, `parent_id`, `intro`) VALUES
+(1, '手机', 0, ''),
+(2, '电脑', 0, ''),
+(3, '小米', 1, ''),
+(5, '小米4', 3, '44445555磊顶戴 11111'),
+(7, '4C', 5, ''),
+(8, '红米', 3, '');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `cms_user`
+--
+
+CREATE TABLE IF NOT EXISTS `cms_user` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `name` char(20) NOT NULL,
+  `pass` char(32) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- 转存表中的数据 `cms_user`
+--
+
+INSERT INTO `cms_user` (`id`, `name`, `pass`) VALUES
+(1, 'admin', '4a7d1ed414474e4033ac29ccb8653d9b');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
